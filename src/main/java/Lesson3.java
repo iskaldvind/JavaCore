69f7bea6526d74e235d4c5fa68ffd8dc4f693c6c
iskaldvind <iskaldvind@gmail.com>
2020-10-23 13:27:39 +0300

    lesson3 refactoring & fixes

--- src/main/java/Lesson3.java
+++ src/main/java/Lesson3.java
@@ -6,31 +6,30 @@ public class Lesson3 {
     public static final Scanner scanner = new Scanner(System.in);
     public static final Random random = new Random();
     public static final String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-    public static final String startingField = "";
-    public static final String hiddenSymbol = "#";
+    public static final String emptyField = "";
+    public static final char hiddenSymbol = '#';
     public static final int fieldSize = 15;
 
     public static void main(String[] args) {
         final String answer = words[random.nextInt(words.length)];
-        final String field = fillField(startingField);
+        final String field = fillField(emptyField);
         gameTurn(field, answer);
     }
 
     public static void gameTurn(String field, String answer) {
         System.out.println("\n\n" + field + "\nType your next guess:");
         final String guess = scanner.nextLine();
-        checkGuess(field, guess, answer);
+        checkGuess(guess, answer);
     }
 
-    public static void checkGuess(String field, String guess, String answer) {
+    public static void checkGuess(String guess, String answer) {
+        final String resultField = openLetters(emptyField, guess, answer);
         if (guess.equals(answer)) {
-            final String openedField = fillField(answer);
-            System.out.println("\n\n" + openedField + "\nCongratulations! You Win the game");
+            System.out.println("\n\n" + resultField + "\nCongratulations! You Win the game");
             return;
         }
-        final String newField = openLetter(startingField, field, guess, answer);
         System.out.println("Nope");
-        gameTurn(newField, answer);
+        gameTurn(resultField, answer);
     }
 
     public static String fillField(String field) {
@@ -38,19 +37,11 @@ public class Lesson3 {
         return fillField(field + hiddenSymbol);
     }
 
-    public static String openLetter(String newField, String field, String guess, String answer) {
-        if (newField.length() == field.length()) return newField;
-        if (newField.length() == answer.length()) return fillField(newField);
+    public static String openLetters(String newField, String guess, String answer) {
+        if (newField.length() == answer.length() || newField.length() == guess.length()) return fillField(newField);
         final char charInAnswer = answer.charAt(newField.length());
-        final char nextChar;
-        if (newField.length() < guess.length()) {
-            final char charInGuess = guess.charAt(newField.length());
-            nextChar = charInGuess != charInAnswer ?
-                    field.charAt(newField.length()) : answer.charAt(newField.length());
-
-        } else {
-            nextChar = field.charAt(newField.length());
-        }
-        return openLetter(newField + nextChar, field, guess, answer);
+        final char charInGuess = guess.charAt(newField.length());
+        final char nextChar = charInGuess == charInAnswer ? charInAnswer : hiddenSymbol;
+        return openLetters(newField + nextChar, guess, answer);
     }
 }
